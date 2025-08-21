cf0ba511f0d96dfaa8f4432a312e8ea8b45fa56a
Sang Duong <sangdev2109@gmail.com>
2025-08-21 14:10:46 +0700

    update ci, cd

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,33 +1,41 @@
+#!/usr/bin/env bash
 #!/bin/bash
-set -e
+set -e  # dừng script nếu lệnh nào fail
 
-BRANCH=${BRANCH:-develop}
-IMAGE=ghcr.io/sangdv219/cat:$BRANCH
-PORT=3000
+APP_NAME=${APP_NAME:-auth}  
+BRANCH=${BRANCH:-master}
+IMAGE=ghcr.io/sangdv219/$APP_NAME:$BRANCH
+HOST_PORT=80
+CONTAINER_PORT=3000
 
-echo "[INFO] Using port: $PORT"
+echo "[INFO] Using container port: $CONTAINER_PORT"
+echo "[INFO] Using host port: $HOST_PORT"
 echo "[INFO] Using branch: $BRANCH"
 echo "[INFO] Deploying $APP_NAME with image $IMAGE"
 
-# Stop old container
-docker stop $APP_NAME || true
-docker rm $APP_NAME || true
+echo "[INFO] Stopping old container if exists..."
+sudo docker stop $APP_NAME || true
+sudo docker rm $APP_NAME || true
 
-# Pull latest image
-docker pull $IMAGE
+echo "[INFO] Log in to GHCR"
 
-# Run new container
-docker run -d --name $APP_NAME \
-  -p 3000:3000 \
-  $IMAGE
+echo "[INFO] Pulling latest image..."
+sudo docker pull $IMAGE
+
+echo "[INFO] Running container..."
+sudo docker run -d --name $APP_NAME -p $HOST_PORT:$CONTAINER_PORT $IMAGE
+
+echo "[INFO] Deployment completed successfully!"
+sudo docker ps -a
+sudo docker logs --tail 20 $APP_NAME
 
 
   # Healthcheck (optional)
-sleep 10
-if curl -fs http://54.252.231.194:3000 >/dev/null; then
-  echo "[INFO] ✅ $APP_NAME is up and healthy on port $APP_PORT"
+sleep 5
+if curl -fs http://54.252.231.194 >/dev/null; then
+  echo "[INFO] ✅ http://54.252.231.194 is up and healthy on port $APP_NAME"
 else
-  echo "[ERROR] ❌ $APP_NAME failed to start" >&2
+  echo "[ERROR] ❌ http://54.252.231.194 failed to start" >&2
   exit 1
 fi
 
